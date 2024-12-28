9beb76abfaf22aee9dd854329e6afe22a9c671e9
almuLR <a.lopezro.2021@alumnos.urjc.es>
2024-12-28 10:11:42 +0100

    Creamos bbdd y los botones de agradecer y mis agradecimientos conducen a sus respectivas pantallas

---
+++ app/src/main/java/com/example/gratefulmind/DataBaseHelper.java
@@ -0,0 +1,54 @@
+package com.example.gratefulmind;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "gratefulMind.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Constructor
+    public DataBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Crear tabla thanks
+        db.execSQL("CREATE TABLE thanks (id INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, gratitude1 TEXT, gratitude2 TEXT, gratitude3 TEXT, lesson TEXT, feeling TEXT, reason TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS thanks");
+        onCreate(db);
+    }
+
+    // Método para obtener todos los agradecimientos guardados
+    public Cursor getGratitudeByDate(String date) {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery("SELECT gratitude1, gratitude2, gratitude3, lesson, feeling, reason FROM thanks ORDER BY date ASC", null);
+    }
+
+    // Método para agregar un agradecimiento a la bbdd
+    public void addGratitude(String date, String gratitude1, String gratitude2, String gratitude3, String lesson, String feeling, String reason) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // Creaamos un objeto ContentValue
+        ContentValues values = new ContentValues();
+        values.put("date", date);
+        values.put("gratitude1", gratitude1);
+        values.put("gratitude2", gratitude2);
+        values.put("gratitude3", gratitude3);
+        values.put("lesson", lesson);
+        values.put("feeling", feeling);
+        values.put("reason", reason);
+
+        // Insertar los valores en la bbdd
+        db.insert("thanks", null, values);
+        db.close();
+    }
+}
--- app/src/main/java/com/example/gratefulmind/MainActivity.java
+++ app/src/main/java/com/example/gratefulmind/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.gratefulmind;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //HelpButton que cambie de pantalla no lo he implementado
+
+        //Implementamos el botón que nos lleve a escribir el agradecimiento
+        Button gratitudeButton = findViewById(R.id.gratitudebutton);
+        gratitudeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(MainActivity.this, GratitudeActivity.class);
+                startActivity(next);
+            }
+        });
+
+
+        //Implementamos el botón que nos lleve a ver todos nuestros agradecimientos
+        Button thanksButton = findViewById(R.id.thanksbutton);
+        thanksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(MainActivity.this, ThanksActivity.class);
+                startActivity(next);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
