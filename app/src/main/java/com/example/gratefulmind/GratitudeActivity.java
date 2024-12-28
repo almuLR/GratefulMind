3399e308509d4143fd9b1b9c27b43c014ca31918
almuLR <a.lopezro.2021@alumnos.urjc.es>
2024-12-28 16:57:28 +0100

    Guardamos en bbdd (Falta probarlo)

--- app/src/main/java/com/example/gratefulmind/GratitudeActivity.java
+++ app/src/main/java/com/example/gratefulmind/GratitudeActivity.java
@@ -1,6 +1,13 @@
 package com.example.gratefulmind;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +17,64 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class GratitudeActivity extends AppCompatActivity {
 
+    private DataBaseHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_gratitude);
+
+        dbHelper = new DataBaseHelper(this);
+
+        //Buscamos campos de texto para guardarnos
+        EditText dateField = findViewById(R.id.editTextDate);
+        EditText gratitudeField1 = findViewById(R.id.editTextTextMultiLine);
+        EditText gratitudeField2 = findViewById(R.id.editTextTextMultiLine2);
+        EditText gratitudeField3 = findViewById(R.id.editTextTextMultiLine3);
+        EditText lessonField = findViewById(R.id.editTextTextMultiLine4);
+        EditText reasonField = findViewById(R.id.editTextTextMultiLine5);
+        RadioGroup feelingGroup = findViewById(R.id.radiogorup);
+
+        //Programamos el guardado de datos presionando botón +
+        ImageButton saveButton = findViewById(R.id.savebutton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String date = dateField.getText().toString().trim();
+                String gratitude1 = gratitudeField1.toString().trim();
+                String gratitude2 = gratitudeField2.toString().trim();
+                String gratitude3 = gratitudeField3.toString().trim();
+                String lesson = lessonField.toString().trim();
+                String reason = reasonField.toString().trim();
+
+                int selectedFeelingId = feelingGroup.getCheckedRadioButtonId();
+                String feeling = null;
+
+                if (selectedFeelingId != -1){
+                    RadioButton selectedFeeling = findViewById(selectedFeelingId);
+                    feeling = selectedFeeling.getText().toString();
+                }
+
+                if (date.isEmpty() || gratitude1.isEmpty() || gratitude2.isEmpty() || gratitude3.isEmpty() || lesson.isEmpty() || feeling == null || reason.isEmpty()){
+                    Toast.makeText(GratitudeActivity.this, "Por favor completa todos los campos.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                dbHelper.addGratitude(date, gratitude1, gratitude2, gratitude3, lesson, feeling, reason);
+                Toast.makeText(GratitudeActivity.this, "¡Agradecimiento registrado exitosamente!", Toast.LENGTH_SHORT).show();
+
+                dateField.setText("");
+                gratitudeField1.setText("");
+                gratitudeField2.setText("");
+                gratitudeField3.setText("");
+                lessonField.setText("");
+                reasonField.setText("");
+                feelingGroup.clearCheck();
+
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
